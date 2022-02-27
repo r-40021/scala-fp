f74d1a8612045bfa7332b7621efdd3e3cb79b904
r-40021 <75155258+r-40021@users.noreply.github.com>
2022-02-27 19:50:31 +0900

    [Add] function Study

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,35 @@
-object Main {
+import scala.annotation.tailrec
 
+case class Switch(isOn: Boolean)
+
+object Main {
+  @tailrec
+  def series(n: Int, acc: Int): Int = {
+    if (n == 0) {
+      acc
+    } else {
+      series(n - 1, acc + n)
+    }
+  }
+
+  @tailrec
+  def fact(n: Int, acc: Int = 1): Int = {
+    if (n <= 1){
+      acc
+    } else {
+      fact(n - 1, acc * n)
+    }
+  }
+
+  def toggle(switch: Switch): Switch = {
+    if (switch.isOn) {
+      Switch(false)
+    } else {
+      Switch(true)
+    }
+  }
+
+  def twice(f: Int => Int): Int => Int = {
+    (n) => f(f(n))
+  }
 }
